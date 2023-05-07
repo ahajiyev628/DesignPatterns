376537b89b166dc29d2d603096641389fac0233b
ahajiyev <ahajiyev628@gmail.com>
2023-05-07 21:03:10 +0400

    added Singleton pattern example

---
+++ src/main/java/org/example/Singleton/Student.java
@@ -0,0 +1,23 @@
+package org.example;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private static Student instance = null;
+    private int id;
+    private String name;
+
+    private Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public static Student getInstance(int id, String name) {
+        if (instance == null) {
+            instance = new Student(id, name);
+        }
+        return instance;
+    }
+}
